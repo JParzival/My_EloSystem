2763382707105abf29888379ffe2ea34bf17f246
jorge <jorge.deandres@hotmail.es>
2018-11-27 20:40:27 +0100

    All the structure is made. Doing the first test I found problems calculating, I should take a look to the funcions and positives/negatives.

---
+++ Imp_Calculator.py
@@ -0,0 +1,29 @@
+# This part calculates the importance of the match depending on the differences of levels.
+# Besides, it has 4 ecuations that make the difference.
+
+import math as m
+
+def higherEloWins(diff):
+
+    result = 1/(0.1*diff)
+    return result
+
+def lowerEloWins(diff):
+
+    result = abs((3/10)*diff + 2)
+    return result
+
+def higherEloLoses(diff):
+
+    result = abs((4/10)*diff + 1)
+    return result
+
+def lowerEloLoses(diff):
+
+    result = 1/(0.1*diff)
+    return result
+
+def draw_both(diff):
+
+    result = ((1/2)*diff)
+    return result
--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
+import Imp_Calculator
+from Imp_Calculator import higherEloLoses, higherEloWins, lowerEloLoses, lowerEloWins, draw_both
 # My personal implementation of an Elo System.
 
-player1 = {'name': "Player One", 'elo': 2000}
-player2 = {'name': "Player Two", 'elo': 2500}
+player1 = {'code':1, 'name': "Player One", 'elo': 2000.0}
+player2 = {'code':2, 'name': "Player Two", 'elo': 2500.0}
 
 # The players are gonna compete
 
@@ -16,33 +18,51 @@ import random
 result = random.uniform(0,1)
 
 
-def calculateEloP1():
+def calculateEloP1(player):
 
     # This means that the Player One has won against the player two
 
-    score = random.uniform(0.5, 1)   # 0.5 winning very close, 1 means maximum difference
+    score = random.uniform(0.5, 1)
 
     lastEloP1 = match[0]['elo']
-    newEloP1 = lastEloP1 + imp * (score - e_score_p1)
+    lastEloP2 = match[1]['elo']
 
-    lastEloP2 = match[0]['elo']
-    newEloP2 = lastEloP2 + imp * ((1-score) - e_score_p2)
+    diff1 = (1-score) - e_score_p1
+    diff2 = score - e_score_p2
+
+    if player['code'] == player1:   # The player with higher elo is P1
+        newEloP1 = lastEloP1 + higherEloWins(diff1) * diff1
+        newEloP2 = lastEloP2 + lowerEloLoses(diff2) * diff2
+    else:
+        k1 = lowerEloWins(diff1)
+        newEloP1 = lastEloP1 + k1 * diff1  
+        k2 = lowerEloLoses(diff2)    
+        newEloP2 = lastEloP2 + k2 * diff2
 
     match[0]['elo'] = newEloP1
     match[1]['elo'] = newEloP2
 
 
-def calculateEloP2():
+def calculateEloP2(player):
 
     # This means that the player 2 has won against the player one
 
     score = random.uniform(0.5, 1)
 
     lastEloP1 = match[0]['elo']
-    newEloP1 = lastEloP1 + imp * ((1-score) - e_score_p1)
+    lastEloP2 = match[1]['elo']
 
-    lastEloP2 = match[0]['elo']
-    newEloP2 = lastEloP2 + imp * (score - e_score_p2)
+    diff1 = (1-score) - e_score_p1
+    diff2 = score - e_score_p2
+
+    if player['code'] == player1:   # The player with higher elo is P1
+        newEloP1 = lastEloP1 + higherEloLoses(diff1) * diff1      
+        newEloP2 = lastEloP2 + lowerEloWins(diff2) * diff2
+    else:
+        k1 = lowerEloLoses(diff1)
+        newEloP1 = lastEloP1 + k1 * diff1
+        k2 = higherEloWins(diff2)      
+        newEloP2 = lastEloP2 + k2 * diff2
 
     match[0]['elo'] = newEloP1
     match[1]['elo'] = newEloP2
@@ -55,8 +75,8 @@ def draw():
     lastEloP1 = match[0]['elo']
     lastEloP2 = match[1]['elo']
 
-    newEloP1 = lastEloP1 + imp * (0.5 - e_score_p1)
-    newEloP2 = lastEloP2 + imp * (0.5 - e_score_p2)
+    newEloP1 = lastEloP1 + draw_both(0.5 - e_score_p1) * (0.5 - e_score_p1)
+    newEloP2 = lastEloP2 + draw_both(0.5 - e_score_p2) * (0.5 - e_score_p2)
 
     match[0]['elo'] = newEloP1
     match[1]['elo'] = newEloP2
@@ -66,23 +86,20 @@ if e_score_p2 > 0.5:         # The second player has greater elo
 
     if result > e_score_p2:
         # P1 has won
-        calculateEloP1()
+        calculateEloP1(player2)
     if result == e_score_p2:
         draw()
     if result < e_score_p2:
-        calculateEloP2()
+        calculateEloP2(player2)
 
 else:                        # The first player has greater elo
 
     if result > e_score_p1:
         # P2 has won
-        calculateEloP2()
+        calculateEloP2(player1)
     if result == e_score_p1:
         draw()
     if result < e_score_p1:
-        calculateEloP1()
+        calculateEloP1(player1)
 
 # Now they have their new elo!
-
-
-
